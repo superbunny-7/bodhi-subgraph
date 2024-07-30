11e4008f9656d8754ac68ba8736de4e33ba40801
superbunny-7 <mflowers03291117@gmail.com>
2024-07-30 12:12:53 +0800

    Update number.ts

--- src/number.ts
+++ src/number.ts
@@ -1,6 +1,6 @@
 import { BigInt, BigDecimal } from "@graphprotocol/graph-ts";
 
-export const TRADERHELPER = "0xb6bcf559e36e2f9ffd0f795b8eeb2d5f90c9c594";
+export const TRADERHELPER = "0xea99a2a055e0ae4d5d5d5a7c7f6819e1eb2bed23";
 export const BI_ZERO = BigInt.fromI32(0);
 export const BI_ONE = BigInt.fromI32(1);
 export const BI_WAD = BigInt.fromI32(10).pow(18);
